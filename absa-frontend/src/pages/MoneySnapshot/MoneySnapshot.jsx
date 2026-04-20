8e03a9cd37e6f107bac03b6d392ff5f618c47f1b
AmberCooper23 <2677396@students.wits.ac.za>
2026-04-20 16:47:17 +0200

    money snapshot css

--- absa-frontend/src/pages/MoneySnapshot/MoneySnapshot.jsx
+++ absa-frontend/src/pages/MoneySnapshot/MoneySnapshot.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-// import "./MoneyShapshot.css";
+import "./MoneySnapshot.css";
 
 function MoneySnapshot() {
 return (
@@ -13,12 +13,10 @@ return (
             <h1 className="storyTitle">
                 Your Financial Story
                 </h1>
-            <p className="storyText">
-                "You're building a strong foundation with 19.7% savings rate. However, your car is eating 21.3% of your take-home pay. Consider whether this aligns with your wealth building goals."
-            </p>
-            <p className="storyText">
-                "Your RA contributions are tax-efficient at R3.5k/month, and you're making progress on your emergency fund."
-            </p>
+            <ul className="storyText">
+            <li>You're building a strong foundation with 19.7% savings rate. However, your car is eating 21.3% of your take-home pay. Consider whether this aligns with your wealth building goals.</li>
+            <li>Your RA contributions are tax-efficient at R3.5k/month, and you're making progress on your emergency fund.</li>
+            </ul>
         </section>
 
         <section className="moneySnapshotCardContainer">
@@ -41,20 +39,20 @@ return (
         <section className="RecentAchievementsContainer">
             {/* <RecentAchievements/> */}
         </section>
-        <section className="ctaCards">
-            <article className="ctaStrategyTrack">
+        <section className="ctaCardContainer">
+            <article className="ctaCard blueCard">
                 <h1 className="ctaTitle">Strategy Track</h1>
                 <p className="ctaText">
                     Select a strategy track for your 5 year journey.
                 </p>
             </article>
-            <article className="ctaSimulations">
+            <article className="ctaCard greenCard">
                 <h1 className="ctaTitle">Run Simulations</h1>
                 <p className="ctaText">
-                    "Test financial decisions with interactive scenarios."
+                    Test financial decisions with interactive scenarios.
                 </p>
             </article>
-            <article className="ctaProgressMap">
+            <article className="ctaCard orangeCard">
                 <h1 className="ctaTitle">Track Milestones</h1>
                 <p className="ctaText">
                     View your progress map and achievements.
