a1743ee812339fd187eba9ff7961f63fd082ed5b
AmberCooper23 <2677396@students.wits.ac.za>
2026-04-20 19:50:43 +0200

    strategy track card styling

--- absa-frontend/src/components/StrategyTrackCard/StrategyTrackCard.jsx
+++ absa-frontend/src/components/StrategyTrackCard/StrategyTrackCard.jsx
@@ -8,17 +8,25 @@ function StrategyTrackCard({ strategyTrack, archetype, description, priorities, 
             <h2 className="stratTrackCardTitle">{strategyTrack}</h2>
             <p className="stratTrackCardArchetype">{archetype}</p>
             </header>
+            <div className="stratTrackCardContent">
             <p className="stratTrackCardDescription">{description}</p>
+            <h4 className="heading">
+                Priorities:
+            </h4>
             <ul className="stratTrackCardPriorities">
                 {priorities.map((priority, index) => (
                     <li key={index}>{priority}</li>
                 ))}
             </ul>
+            <h4 className="heading">
+                5 Year Milestones:
+            </h4>
             <ul className={`stratTrackCardMilestones ${className || ""}`}>
                 {milestones.map((milestone, index) => (
                     <li key={index}>{milestone}</li>
                 ))}
             </ul>
+            </div>
         </article>
     );
 }
