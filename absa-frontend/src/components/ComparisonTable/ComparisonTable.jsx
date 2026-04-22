063ebac345f7837cf32b570e12978ae4da971816
AmberCooper23 <2677396@students.wits.ac.za>
2026-04-22 15:24:37 +0200

    comparison table styling

--- absa-frontend/src/components/ComparisonTable/ComparisonTable.jsx
+++ absa-frontend/src/components/ComparisonTable/ComparisonTable.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import "./ComparisonTable.css";
 
 function ComparisonTable() {
     return (
-        <table className="comparisonTable">
+        <article className="comparisonTableCard">
+           <table className="comparisonTable">
+            <caption className="comparisonTableTitle">
+                Track Comparison
+            </caption>
             <thead className="comparisonTableHeadRow" >
                 <tr>
                     <th className="comparisonTableHeadCell">
@@ -26,9 +31,9 @@ function ComparisonTable() {
             <tbody className="comparisonTableBody">
                 <tr className="comparisonTableRow">
                     <td className="comparisonTableTrack">The Builder</td>
-                    <td className="comparisonTableBestFor">First time property buyers</td>
-                    <td className="comparisonTableRiskLevel">Low</td>
-                    <td className="comparisonTableTimeCommitment">High(saving focus)</td>
+                    <td className="comparisonTableCell">First time property buyers</td>
+                    <td className="comparisonTableRiskLevel"><span className="riskLow">Low</span></td>
+                    <td className="comparisonTableCell">High(saving focus)</td>
                     <td className="comparisonTableGoal">Own Property</td>
                 </tr>
             </tbody>
@@ -36,9 +41,9 @@ function ComparisonTable() {
              <tbody className="comparisonTableBody">
                 <tr className="comparisonTableRow">
                     <td className="comparisonTableTrack">The Explorer</td>
-                    <td className="comparisonTableBestFor">Balance seekers</td>
-                    <td className="comparisonTableRiskLevel">Medium</td>
-                    <td className="comparisonTableTimeCommitment">Medium (balanced)</td>
+                    <td className="comparisonTableCell">Balance seekers</td>
+                    <td className="comparisonTableRiskLevel"><span className="riskMedium"> Medium </span></td>
+                    <td className="comparisonTableCell">Medium (balanced)</td>
                     <td className="comparisonTableGoal">R250k portfolio, 20% offshore</td>
                 </tr>
             </tbody>
@@ -46,14 +51,15 @@ function ComparisonTable() {
             <tbody className="comparisonTableBody">
                 <tr className="comparisonTableRow">
                     <td className="comparisonTableTrack">The Maverick</td>
-                    <td className="comparisonTableBestFor">Aggressive investors</td>
-                    <td className="comparisonTableRiskLevel">High</td>
-                    <td className="comparisonTableTimeCommitment">Low (automated)</td>
+                    <td className="comparisonTableCell">Aggressive investors</td>
+                    <td className="comparisonTableRiskLevel"><span className="riskHigh">High</span></td>
+                    <td className="comparisonTableCell">Low (automated)</td>
                     <td className="comparisonTableGoal">R500k, 40% offshore</td>
                 </tr>
             </tbody>
-            
-        </table>
+        </table> 
+        </article>
+        
     );
 }
 
--- absa-frontend/src/pages/StrategyTrackPage/StrategyTrack.jsx
+++ absa-frontend/src/pages/StrategyTrackPage/StrategyTrack.jsx
@@ -79,6 +79,7 @@ function StrategyTrack() {
         </section>
 
         <section className="trackComparisonContainer">
+            
             <ComparisonTable/>
         </section>
 
