e16b0bf45badd727f1176877bde9a2e70f346055
AmberCooper23 <2677396@students.wits.ac.za>
2026-04-22 16:38:38 +0200

    finance context setup

---
+++ absa-frontend/src/context/FinanceContext.js
@@ -0,0 +1,54 @@
+import React, { createContext, useState } from "react";
+
+const FinanceContext = createContext();
+
+function FinanceProvider({children}) {
+
+    // Income & deductions
+        const [income, setIncome] = useState(0);
+        const [tax, setTax] = useState(0);
+        const [ra, setRa] = useState(0);
+        const [medicalAid, setMedicalAid] = useState(0);
+
+    // Savings & Investments
+        const [savingsRate, setSavingsRate] = useState(0);
+        const [emergencyFund, setEmergencyFund] = useState(0);
+        const [tfsa, setTfsa] = useState(0);
+        const [investments, setInvestments] = useState(0);
+
+    // Debt
+        const [studentLoan, setStudentLoan] = useState(0);
+        const [homeLoan, setHomeLoan] = useState(0);
+        const [personalLoan, setPersonalLoan] = useState(0);
+        const [carLoan, setCarLoan] = useState(0);
+
+    // Derived values
+        const takeHome = income - tax - ra - medicalAid;
+        const totalDebt = studentLoan + homeLoan + personalLoan + carLoan
+
+        return (
+            <FinanceProvider
+            value={{
+                income, setIncome,
+                tax, setTax,
+                ra, setRa, 
+                medicalAid, setMedicalAid,
+                savingsRate, setSavingsRate,
+                emergencyFund, setEmergencyFund,
+                tfsa, setTfsa,
+                investments, setInvestments,
+                studentLoan, setStudentLoan,
+                homeLoan, setHomeLoan,
+                personalLoan, setPersonalLoan,
+                carLoan, setCarLoan,
+                takeHome,
+                totalDebt
+            }}
+            >
+                {children}
+            </FinanceProvider>
+        );
+}
+
+export default FinanceContext;
+export default {FinanceProvider};
